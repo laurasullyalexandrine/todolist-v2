aa1c80cc676af0ea39b808db28641c1f3a93a376
laurasullyalexandrine <launyie@gmail.com>
2024-02-16 15:11:44 +0100

    managing adding a task

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
 use App\Repository\TaskRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/tasks', name: 'task_')]
 class TaskController extends AbstractController
@@ -15,6 +19,27 @@ class TaskController extends AbstractController
     {
         return $this->render('task/list.html.twig', [
             'tasks' => $taskRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/', name: 'create')]
+    public function create(Request $request, EntityManagerInterface $manager): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $manager->persist($task);
+            $manager->flush();
+
+            $this->addFlash('success', 'La tâche a été bien été ajoutée.');
+
+            return $this->redirectToRoute('task_list');
+        }
+        return $this->render('task/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
