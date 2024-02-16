e3eebf4bbfea1184dfbb61fddb8b2c36687d11c1
laurasullyalexandrine <launyie@gmail.com>
2024-02-16 12:19:28 +0100

    adding User controller and list method

---
+++ src/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/users', name: 'users_')]
+class UserController extends AbstractController
+{
+    #[Route('/list', name: 'list', methods: ["GET"])]
+    public function list(UserRepository $userRepository): Response
+    {
+        return $this->render('user/list.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+}
