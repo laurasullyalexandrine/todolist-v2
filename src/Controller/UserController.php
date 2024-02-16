f142382eaf299bb6f53e581e2a831bf01ec2f4cb
laurasullyalexandrine <launyie@gmail.com>
2024-02-16 14:01:32 +0100

    updating access to the users list page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/users', name: 'users_')]
 class UserController extends AbstractController
 {
-    #[Route('/list', name: 'list', methods: ["GET"])]
+    #[Route('/', name: 'list', methods: ["GET"])]
     public function list(UserRepository $userRepository): Response
     {
         return $this->render('user/list.html.twig', [
