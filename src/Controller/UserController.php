85618af740137e07f2ecafae6b4153dadab0ba2b
laurasullyalexandrine <launyie@gmail.com>
2024-02-23 14:13:34 +0100

    finalization of the user management process env admin and change order fields

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,20 +2,101 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-#[Route('/users', name: 'users_')]
+#[Route('/admin/users', name: 'admin_users_')]
+#[IsGranted('ROLE_ADMIN')]
 class UserController extends AbstractController
 {
+    public function __construct(
+        private UserPasswordHasherInterface $hasher,
+        private EntityManagerInterface $manager,
+    ) {
+    }
     #[Route('/', name: 'list', methods: ["GET"])]
     public function list(UserRepository $userRepository): Response
     {
         return $this->render('user/list.html.twig', [
             'users' => $userRepository->findAll(),
+        ]);
+    }
+
+
+    #[Route('/create', name: 'create', methods: ["GET", "POST"])]
+    public function create(Request $request): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $user->setPassword(
+                    $this->hasher->hashPassword(
+                        $user,
+                        $form->get('password')->getData()
+                    )
+                );
+
+                $this->manager->persist($user);
+                $this->manager->flush();
+
+                $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+                return $this->redirectToRoute('user_list');
+            } catch (\Exception $e) {
+                $this->addFlash('error', 'Une erreur s\'est produite lors de l\'ajout du nouvel utilisateur  ' . $e->getMessage());
+                return $this->redirect($request->headers->get('referer'));
+            }
+        }
+        return $this->render('user/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ["GET", "POST"])]
+    public function edit(
+        User $user,
+        Request $request
+    ) {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $roles = $form->get('roles')->getData();
+
+            try {
+                $user->setRoles([$roles[0]])
+                    ->setPassword(
+                        $this->hasher->hashPassword(
+                            $user,
+                            $form->get('password')->getData()
+                        )
+                    );
+
+                $this->manager->persist($user);
+                $this->manager->flush();
+
+                $this->addFlash('success', "L'utilisateur a bien été modifié.");
+                return $this->redirectToRoute('admin_users_list');
+            } catch (\Exception $e) {
+                $this->addFlash('error', 'Une erreur s\'est produite lors de l\'ajout du nouvel utilisateur  ' . $e->getMessage());
+                return $this->redirect($request->headers->get('referer'));
+            }
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
         ]);
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('roles', ChoiceType::class, [
+                'label' =>  'Choisir le rôle de l\'utilisateur',
+                'multiple' => true,
+                'expanded' => true,
+                'attr' => [
+                    'class' => 'mb-3',
+                ],
+                'choices' => [
+                    'utilisateur' => 'ROLE_USER',
+                    'administrateur' => 'ROLE_ADMIN',
+                ]
+            ])
+            ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email'
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les deux mots de passe doivent correspondre.',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe ',
+                ],
+                'second_options' => [
+                    'label' => 'Tapez le mot de passe à nouveau',
+                ],
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
