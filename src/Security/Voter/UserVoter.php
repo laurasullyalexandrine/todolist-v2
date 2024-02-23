a50995d4fdf3c549c5d8f982f774410603d3f203
laurasullyalexandrine <launyie@gmail.com>
2024-02-23 14:32:45 +0100

    adding UserVoter

---
+++ src/Security/Voter/UserVoter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserVoter extends Voter
+{
+    public const READ = 'GET_READ';
+    public const EDIT = 'POST_EDIT';
+    public const DELETE = 'POST_DELETE';
+    public const NEW = 'POST_NEW';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::READ, self::EDIT, self::DELETE, self::NEW])
+            && $subject instanceof \App\Entity\User;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case self::READ:
+                if(in_array("ROLE_ADMIN", $user->getRoles())) {
+                    return true;
+                }
+                break;
+            case self::EDIT:
+                if(in_array("ROLE_ADMIN", $user->getRoles())) {
+                    return true;
+                }
+                break;
+            case self::NEW:
+                if(in_array("ROLE_ADMIN", $user->getRoles())) {
+                    return true;
+                }
+                break;
+        }
+
+        return false;
+    }
+}
