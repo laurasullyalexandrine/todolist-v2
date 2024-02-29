64f86c7a7bfed710801a9c715f2efce233c1b80c
laurasullyalexandrine <launyie@gmail.com>
2024-02-29 15:34:08 +0100

    running unit tests on task entity and adding coverage report

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -5,23 +5,123 @@ namespace App\Tests\Unit;
 use App\Entity\Task;
 use App\Entity\User;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class TaskTest extends TestCase
 {
-    public function testSomething(UserPasswordHasherInterface $hasher): void
+    public function testChangeTaskStatus(): void
     {
+        $now = new \DateTimeImmutable();
+        $date = $now->format('d-m-Y');
+
         $user = new User();
         $user->setUsername('Jean')
             ->setEmail('jean.louis@todolist.fr')
-            ->setPassword(
-                $hasher->hashPassword(
-                    $user,
-                    'password'
-                )
-            );
+            ->setPassword("password");
 
-        $task = new Task('Tache test', 'Contenu tâche test', "1", $user);
-        $this->assertTrue(0);
+
+        $task = new Task();
+        $task->setTitle('Tache test')
+            ->setContent('Contenu tâche test')
+            ->setIsDone(false)
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setUser($user);
+
+        $this->assertEquals($date, $task->getCreatedAt()->format('d-m-Y'));
+
+        $this->assertFalse($task->isIsDone());
+
+        $task->toggle(true);
+
+        $task->setUpdatedAt(new \DateTimeImmutable());
+
+        $this->assertTrue($task->isIsDone());
+        $this->assertEquals($date, $task->getUpdatedAt()->format('d-m-Y'));
+    }
+
+
+    public function testIfTaskIsDone(): void
+    {
+        $user = new User();
+        $user->setUsername('Pierre')
+            ->setEmail('pierre.bachelet@todolist.fr')
+            ->setPassword("password");
+
+
+        $task = new Task();
+        $task->setTitle('Tâche test')
+            ->setContent('Contenu tâche test')
+            ->setIsDone(true)
+            ->setUser($user);
+
+        $this->assertTrue($task->isIsDone());
+
+        $this->assertEquals(true, $task->isIsDone());
+    }
+
+    public function testGetUserFromTask(): void
+    {
+        $user = new User();
+        $user->setUsername('David')
+            ->setEmail('david.charvet@todolist.fr')
+            ->setPassword("password");
+
+
+        $task = new Task();
+        $task->setTitle('Tâche test')
+            ->setContent('Contenu tâche test')
+            ->setIsDone(true)
+            ->setUser($user);
+
+        $this->assertSame($user, $task->getUser());
+    }
+
+    public function testGetTitleTask(): void
+    {
+        $user = new User();
+        $user->setUsername('François')
+            ->setEmail('françois.valerie@todolist.fr')
+            ->setPassword("password");
+
+        $task = new Task();
+        $task->setTitle('Tâche test')
+            ->setContent('Contenu tâche test')
+            ->setIsDone(true)
+            ->setUser($user);
+
+        $this->assertSame('Tâche test', $task->getTitle());
+    }
+
+    public function testGetContentTask(): void
+    {
+        $user = new User();
+        $user->setUsername('Paul')
+            ->setEmail('paul.valerie@todolist.fr')
+            ->setPassword("password");
+
+        $task = new Task();
+        $task->setTitle('Tâche test')
+            ->setContent('Contenu tâche test')
+            ->setIsDone(true)
+            ->setUser($user);
+
+        $this->assertSame('Contenu tâche test', $task->getContent());
+    }
+
+    public function testTaskToString(): void
+    {
+        $user = new User();
+        $user->setUsername('Jacques')
+            ->setEmail('jacques.chirac@todolist.fr')
+            ->setPassword("password");
+
+        $task = new Task();
+        $task->setTitle('Tâche test')
+            ->setContent('Contenu tâche test')
+            ->setIsDone(true)
+            ->setUser($user);
+
+        $tastkString = $task->__toString($task->getTitle());
+
+        $this->assertSame($task->getTitle(), $tastkString);
     }
 }
