8dbabbfb044fc7a78180b0c230696a863f16f046
laurasullyalexandrine <launyie@gmail.com>
2024-02-29 11:58:29 +0100

    update adding user anonymous and tasks for admin and anonymous

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -19,27 +19,66 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
 
-        // Administrateur
+        // Administrator
         $admin = new User();
         $admin->setUsername('laura')
-            ->setEmail('laura@gmail.com')
+            ->setEmail('laura@todolist.fr')
             ->setRoles(['ROLE_ADMIN'])
             ->setPassword(
                 $this->hasher->hashPassword(
                     $admin,
-                    "password"
+                    "admin"
                 )
             )
             ->setCreatedAt(new \DateTimeImmutable());
 
+        for ($t = 1; $t <= 15; $t++) {
+            $taskAdmin = new Task();
+            $taskAdmin->setTitle('Tâche administration ' . $t)
+                ->setContent('Contenu pour les tâches d\'administration ' . $t)
+                ->setIsDone(false)
+                ->setCreatedAt(new \DateTimeImmutable());
+
+            $admin->addTask($taskAdmin);
+
+            $manager->persist($taskAdmin);
+        }
+
         $manager->persist($admin);
 
+        // Anonymous
+        $anonymous = new User();
+        $anonymous->setUsername('anonymous')
+            ->setEmail('anonymous@todolist.fr')
+            ->setRoles(['ROLE_ANONYMUS'])
+            ->setPassword(
+                $this->hasher->hashPassword(
+                    $anonymous,
+                    "anonymous"
+                )
+            )
+            ->setCreatedAt(new \DateTimeImmutable());
 
-        // Utilisateurs
-        for ($u = 1; $u <= 500; $u++) {
+        for ($t = 1; $t <= 15; $t++) {
+            $taskAnonymous = new Task();
+            $taskAnonymous->setTitle('Tâche anonyme ' . $t)
+                ->setContent('Contenu pour les tâches anonymes ' . $t)
+                ->setIsDone(false)
+                ->setCreatedAt(new \DateTimeImmutable());
+
+            $anonymous->addTask($taskAnonymous);
+
+            $manager->persist($taskAnonymous);
+        }
+
+        $manager->persist($anonymous);
+
+
+        // Users
+        for ($u = 1; $u <= 200; $u++) {
             $user = new User();
             $user->setUsername($faker->userName())
-                ->setEmail($user->getUsername() . $faker->safeEmailDomain())
+                ->setEmail($user->getUsername() . '@todolist.fr')
                 ->setRoles(['ROLE_USER'])
                 ->setCreatedAt(new \DateTimeImmutable())
                 ->setPassword(
@@ -50,10 +89,10 @@ class AppFixtures extends Fixture
                 );
 
             // Les tâches
-            for ($t= 1; $t <= 50; $t++) {
+            for ($t = 1; $t <= 15; $t++) {
                 $task = new Task();
-                $task->setTitle('Tâche ' . $t)
-                    ->setContent('Contenu pour les tâches ' . $t)
+                $task->setTitle('Tâche utilisateur' . $t)
+                    ->setContent('Contenu pour les tâches des utilisateurs' . $t)
                     ->setIsDone(false)
                     ->setCreatedAt(new \DateTimeImmutable());
 
@@ -64,7 +103,6 @@ class AppFixtures extends Fixture
 
             $manager->persist($user);
         }
-
         $manager->flush();
     }
 }
